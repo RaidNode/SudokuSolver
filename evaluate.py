07255efe8ad8c8a0de06df756d30e460969d45ea
Joshua Mankelow <joshua@mankelow.org>
2017-09-29 10:05:19 +0100

    Added barebones functions and new file

---
+++ evaluate.py
@@ -0,0 +1,25 @@
+def evaluate_row(sudoku, x):
+
+    # Return True for valid row
+    # Return False for invalid row
+
+    # Do stuff
+    return sudoku[x]
+
+
+def evaluate_column(sudoku, y):
+
+    # Return True for valid column
+    # Return False for invalid column
+
+    # Do stuff
+    return sudoku[y]
+
+
+def evaluate_block(sudoku, x, y):
+
+    # Return True for valid block
+    # Return False for invalid block
+
+    # Do stuff
+    return sudoku[x,y]
--- main.py
+++ main.py
@@ -3,5 +3,3 @@ grid = []
 for i in range(0, 9):
 
     grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-print(grid)
