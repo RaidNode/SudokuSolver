fab0457d0ff6bd72b09a10d63df92c86a7f56622
Joshua Mankelow <joshua@mankelow.org>
2017-09-30 20:51:39 +0100

    Added functions to check row validity

--- evaluate.py
+++ evaluate.py
@@ -3,9 +3,43 @@ def evaluate_row(sudoku, x):
     # Return True for valid row
     # Return False for invalid row
 
-    # Do stuff
-    return sudoku[x]
+    number_list = []
 
+    # Generate a list of numbers from the numbers in the sudoku
+    for number in sudoku[x]:
+
+        number_list.append(number)
+
+    # Check if every number 1 to 9 appears in the string
+    for i in range(1, 10):
+
+        if i not in number_list:
+
+            return False
+
+    return True
+
+# This one is probably quicker
+def evaluate_row2(sudoki, x):
+
+    # Return True for valid row
+    # Return Fasle for invalid row
+
+
+    number_list = []
+
+    # Generate a list of numbers from the numbers in the sudoku
+    for number in sudoku[x]:
+
+        if number in number_list:
+
+            return False
+
+        else:
+
+            number_list.append(number)
+
+    return True
 
 def evaluate_column(sudoku, y):
 
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+# With regards to the grid, let it be said that if we're referencing it
+# using the grid[x][y] syntax, it will be rotated 90 degrees when printed.
+# Logically, it will function the same it just may be a little confusing
+# when trying to read the sudoku in printed form
+
 grid = []
 
 for i in range(0, 9):
 
     grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+for x in grid:
+
+    print(x)
