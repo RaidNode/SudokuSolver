a6e6d79354e3b31a767da48196d2a4b52a866fd5
connorprice <conpeterprice@gmail.com>
2017-10-01 14:13:35 +0100

    Added functions to interface with the UI. Added some code into main.py so that the UI opens when it is run.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+# Module Imports
+import sys
+from PyQt5 import QtWidgets
+
+# Local Imports
+import ui
+
+app = QtWidgets.QApplication(sys.argv)
+window = ui.MainWindow()
+
 grid = []
 for i in range(0, 9):
-    grid[i].append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+
+def solve():
+    print(window.get_grid_value(0, 1))
+
+
+window.solveButton.clicked.connect(solve)
+app.exec_()
