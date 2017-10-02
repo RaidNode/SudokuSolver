f4fb0745a5d9930dc4b4d5f4f8c6839358c7a8e1
Joshua Mankelow <joshua@mankelow.org>
2017-10-02 10:42:46 +0100

    Added function to check blocks

--- evaluate.py
+++ evaluate.py
@@ -41,8 +41,31 @@ def evaluate_column(sudoku, y):
 
 def evaluate_block(sudoku, x, y):
 
+    from math import ceil
+
     # Return True for valid block
     # Return False for invalid block
 
-    # Do stuff
-    return sudoku[x,y]
+    number_list = []
+
+    # Find block x and block y
+    block_x = ceil(x / 3) - 1
+    block_y = ceil(y / 3) - 1
+
+    # Find lower limits for x and y
+    lower_x = int(block_x * 3)
+    lower_y = int(block_y * 3)
+
+    for x in range(lower_x, lower_x + 2):
+
+        for y in range(lower_y, lower_y + 2):
+
+            if sudoku[x][y] in number_list and sudoku[x][y] is not None:
+
+                return False
+
+            else:
+
+                number_list.append(sudoku[x][y])
+
+    return True
