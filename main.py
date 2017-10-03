13e89bb934842b537a2885cdba20f871730ca326
connorprice <conpeterprice@gmail.com>
2017-10-03 20:44:08 +0100

    Accidentally left test code in which made it return the same value for every grid space.

--- main.py
+++ main.py
@@ -18,15 +18,14 @@ for i in range(0, 9):
     grid.append([None, None, None, None, None, None, None, None, None])
 
 def solve():
-    for grid_column in grid:  # Go through the columns of the grid.
-        for grid_column_y in range(len(grid_column)):  # Go through the individual numbers in the column.
-            grid_value = window.get_grid_value(0, 1)  # Get the value on the UI.
+    for x, grid_column in enumerate(grid):  # Go through the columns of the grid.
+        for y in range(len(grid_column)):  # Go through the individual numbers in the column.
+            grid_value = window.get_grid_value(x, y)  # Get the value on the UI.
             if grid_value == "":  # If it is blank...
-                grid_column[grid_column_y] = None  # ...put nothing...
+                grid_column[y] = None  # ...put nothing...
             else:  # ...or if there is something...  ( We don't need to validate because we do that in the UI. )
-                grid_column[grid_column_y] = int(grid_value)  # ...put the number in.
-
-            print(grid_column[grid_column_y])  # Print for testing.
+                grid_column[y] = int(grid_value)  # ...put the number in.
+    print( grid )
     return
 
 window.solveButton.clicked.connect(solve)  # Attach the solve function to the button on the UI.
